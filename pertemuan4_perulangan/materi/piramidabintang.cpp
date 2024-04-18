a1f7145fbf253f8b0a121240c862f544aa54238f
chyntiaclaudiaa <chyntiaclaudia1809@gmail.com>
2024-04-18 17:44:14 +0700

    lab AP 5

---
+++ pertemuan4_perulangan/materi/piramidabintang.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    system("cls");
+
+    int tinggi;
+    cout << "input tinggi";
+    cin >> tinggi;
+
+    for (int i = 0; i < tinggi; i++)
+    {
+        for (int j = tinggi; j > i + 1; i++)
+        {
+            cout << " ";
+        }
+        for (int k = 0; k <= i; k++)
+        {
+            cout << "* ";
+        }
+        cout << endl;
+    }
+
+    return 0;
+}
---
+++ pertemuan4_perulangan/materi/segitigabintang.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int tinggi;
+    cout << "Input tinggi segitiga: ";
+    cin >> tinggi;
+
+    for (int i = tinggi; i > 0; i--)
+    {
+        for (int j = 0; j < i; j++)
+        {
+            cout << tinggi << " ";
+        }
+        cout << endl; // Pindah ke baris berikutnya setelah setiap baris bintang
+    }
+
+    return 0;
+}
